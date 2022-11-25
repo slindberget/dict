70ddba0322db95214d9538df572e2458003ae6f4
slindberget <118910064+slindberget@users.noreply.github.com>
2022-11-25 14:23:06 +0100

    conn to connection

--- dict.py
+++ dict.py
@@ -1,53 +1,53 @@
 import psycopg2
-conn = psycopg2.connect(
+connection = psycopg2.connect(
    host="localhost",
    database="dict",
    user="postgres",
    password="XXXXX"
 )
 
-def read_dict(conn):
-    cur = conn.cursor()
+def read_dict(connection):
+    cur = connection.cursor()
     cur.execute("SELECT * FROM dictionary2;")
     cur.execute("COMMIT;")
     rows = cur.fetchall()
     cur.close()
-    conn.close()
+    connection.close()
     return rows
-def add_word(conn, word, translation):
-    cur = conn.cursor()
+def add_word(connection, word, translation):
+    cur = connection.cursor()
     cur.execute(f"INSERT INTO dictionary2 (word, translation) VALUES ('{word}', '{translation}');")
     cur.execute("COMMIT;")
     cur.close()
-    conn.close()
-def delete_word(conn, ID):
-    cur = conn.cursor()
+    connection.close()
+def delete_word(connection, ID):
+    cur = connection.cursor()
     cur.execute(f"DELETE FROM dictionary2 WHERE id = '{ID}';")
     cur.execute("COMMIT;")
     cur.close()
-    conn.close()
-def save_dict(conn):
-    cur = conn.cursor()
+    connection.close()
+def save_dict(connection):
+    cur = connection.cursor()
     cur.execute("COMMIT;")
     cur.close()
-    conn.close()
+    connection.close()
 
-def insert_word(conn, word, translation):
+def insert_word(connection, word, translation):
     print(word, translation)
 
 while True: ## REPL - Read Execute Program Loop
     cmd = input("HELLO\nUse the following list of commands\nAdd = Add word and translation\nDelete = Input id to delete row\nQuit = Exit program\nCommand: ").lower()
     if cmd == "list":
-        print(read_dict(conn))
+        print(read_dict(connection))
     elif cmd == "add":
         word = input("  Word: ")
         translation = input("  Translation: ")
-        add_word(conn, word, translation)
+        add_word(connection, word, translation)
     elif cmd == "delete":
         ID = input("  ID: ")
-        delete_word(conn, ID)
+        delete_word(connection, ID)
     elif cmd == "quit":
-        save_dict(conn)
+        save_dict(connection)
         exit()
 
 
